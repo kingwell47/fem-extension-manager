bc2a746039ed892dfcb5113eb959b8eee9171a3c
kingwell47 <joeldoctor47@gmail.com>
2025-03-27 07:15:35 +0800

    initial theme customization

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import MainPage from "./pages/MainPage";
 
 function App() {
+  const currentTheme = "fem-dark";
+
   return (
-    <div data-theme="fem-dark" className="px-4 py-6">
+    <div data-theme={currentTheme} className="px-4 py-6">
       <MainPage />
     </div>
   );
