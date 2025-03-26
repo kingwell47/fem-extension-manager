569c326d4ad38db8726d0c0b38b132048637b161
kingwell47 <joeldoctor47@gmail.com>
2025-03-26 22:12:20 +0800

    Made initial Top Bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <>
+    <div data-theme="fem-dark" className="px-4 py-6">
       <MainPage />
-    </>
+    </div>
   );
 }
 
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Logo from "../assets/images/logo.svg";
+import SunIcon from "../assets/images/icon-sun.svg";
 
 const TopBar = () => {
-  return <div>TopBar</div>;
+  return (
+    <div className="navbar bg-base-100 shadow-sm rounded-lg h-16.5 p-3">
+      <div className="flex-1">
+        <img src={Logo} alt="Extensions" />
+      </div>
+      <div className="flex-none">
+        <button className="btn btn-square rounded-xl size-12.5">
+          <img src={SunIcon} alt="Light Mode" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TopBar;
